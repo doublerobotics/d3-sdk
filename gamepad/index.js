6ca9da6a5d6b1ebd8e2b16f7bb8bc1574dce8008
David Cann <david@davidcann.com>
2019-01-05 14:40:47 -0800

    Saving stuff

--- gamepad/index.js
+++ gamepad/index.js
@@ -10,25 +10,15 @@ var controller = dualShock({
 });
 controller.on('error', err => console.log(err));
 
-// Events
+// Connection
 controller.on('connected', () => console.log('connected'));
 controller.on('connection:change', data => console.log(data));
 
+// Sticks
 controller.on('left:move', data => console.log('left Moved: ' + data.x + ' | ' + data.y));
 controller.on('right:move', data => console.log('right Moved: ' + data.x + ' | ' + data.y));
 
-controller.on('square:press', ()=> console.log('square press'));
-controller.on('square:release', () => console.log('square release'));
-
-controller.on('triangle:press', ()=> console.log('triangle press'));
-controller.on('triangle:release', () => console.log('triangle release'));
-
-controller.on('circle:press', ()=> console.log('circle press'));
-controller.on('circle:release', () => console.log('circle release'));
-
-controller.on('x:press', ()=> console.log('x press'));
-controller.on('x:release', () => console.log('x release'));
-
+// D-Pad
 controller.on("dpadUp:press", () => console.log("dpadUp:press") );
 controller.on("dpadUpRight:press", () => console.log("dpadUpRight:press") );
 controller.on("dpadRight:press", () => console.log("dpadRight:press") );
@@ -37,10 +27,6 @@ controller.on("dpadDown:press", () => console.log("dpadDown:press") );
 controller.on("dpadDownLeft:press", () => console.log("dpadDownLeft:press") );
 controller.on("dpadLeft:press", () => console.log("dpadLeft:press") );
 controller.on("dpadUpLeft:press", () => console.log("dpadUpLeft:press") );
-controller.on('l1:press', () => console.log('l1:press') );
-controller.on('l2:press', () => console.log('l2:press') );
-controller.on('r1:press', () => console.log('r1:press') );
-controller.on('r2:press', () => console.log('r2:press') );
 
 controller.on("dpadUp:release", () => console.log("dpadUp:release") );
 controller.on("dpadUpRight:release", () => console.log("dpadUpRight:release") );
@@ -50,7 +36,23 @@ controller.on("dpadDown:release", () => console.log("dpadDown:release") );
 controller.on("dpadDownLeft:release", () => console.log("dpadDownLeft:release") );
 controller.on("dpadLeft:release", () => console.log("dpadLeft:release") );
 controller.on("dpadUpLeft:release", () => console.log("dpadUpLeft:release") );
+
+// Buttons
+controller.on('square:press', ()=> console.log('square press'));
+controller.on('square:release', () => console.log('square release'));
+controller.on('triangle:press', ()=> console.log('triangle press'));
+controller.on('triangle:release', () => console.log('triangle release'));
+controller.on('circle:press', ()=> console.log('circle press'));
+controller.on('circle:release', () => console.log('circle release'));
+controller.on('x:press', ()=> console.log('x press'));
+controller.on('x:release', () => console.log('x release'));
+
+controller.on('l1:press', () => console.log('l1:press') );
+controller.on('l2:press', () => console.log('l2:press') );
+controller.on('r1:press', () => console.log('r1:press') );
+controller.on('r2:press', () => console.log('r2:press') );
 controller.on('l1:release', () => console.log('l1:release') );
 controller.on('l2:release', () => console.log('l2:release') );
 controller.on('r1:release', () => console.log('r1:release') );
 controller.on('r2:release', () => console.log('r2:release') );
+
